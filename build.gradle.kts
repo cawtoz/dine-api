e3c59a7d6bba5cf94162a03fc28651b0db08a477
cawtoz <cawtoz.dev@gmail.com>
2024-08-30 13:34:44 -0500

    Configured project build and run

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.serialization") version "2.0.0"
+    application
 }
 
 version = "1.0.0"
@@ -9,6 +10,7 @@ group = "com.github.cawtoz"
 val ktorVersion = "2.3.12"
 val exposedVersion = "0.53.0"
 val postgresqlVersion = "42.7.4"
+
 
 repositories {
     mavenCentral()
@@ -27,4 +29,8 @@ dependencies {
 
 kotlin {
     jvmToolchain(20)
+}
+
+application {
+    mainClass.set("com.github.cawtoz.dineapi.ApplicationKt")
 }
