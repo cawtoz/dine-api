07036d30071ad843e21c48c27d5aa7a0e7cb4da6
cawtoz <cawtoz.dev@gmail.com>
2024-09-02 23:26:52 -0500

    Added deleteByMenuId method to foodFAO

--- src/main/kotlin/com/github/cawtoz/dineapi/modules/database/dao/FoodDAO.kt
+++ src/main/kotlin/com/github/cawtoz/dineapi/modules/database/dao/FoodDAO.kt
@@ -2,6 +2,10 @@ package com.github.cawtoz.dineapi.modules.database.dao
 
 import com.github.cawtoz.dineapi.modules.database.tables.Foods
 import com.github.cawtoz.dineapi.modules.models.Food
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.sql.SQLException
 
 class FoodDAO: GenericDAO<Food>(
     "Food",
@@ -22,5 +26,18 @@ class FoodDAO: GenericDAO<Food>(
             row[Foods.price]
         )
     }
-)
+) {
+
+    fun deleteByMenuId(menuId: Int): Boolean {
+        return try {
+            transaction {
+                Foods.deleteWhere { Foods.menuId eq menuId } > 0
+            }
+        } catch (e: SQLException) {
+            println("Error deleting $entityName by id: ${e.message}")
+            false
+        }
+    }
+
+}
 
