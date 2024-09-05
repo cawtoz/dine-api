c077ea36fc08bebb7c9f23a3864325389fdf0bb1
cawtoz <cawtoz.dev@gmail.com>
2024-09-04 20:36:56 -0500

    Implemented CRUD routes for Menu

--- src/main/kotlin/com/github/cawtoz/dineapi/modules/routes/RoutesModule.kt
+++ src/main/kotlin/com/github/cawtoz/dineapi/modules/routes/RoutesModule.kt
@@ -1,10 +1,13 @@
 package com.github.cawtoz.dineapi.modules.routes
 
 import com.github.cawtoz.dineapi.modules.database.dao.FoodDAO
+import com.github.cawtoz.dineapi.modules.database.dao.MenuDAO
 import com.github.cawtoz.dineapi.modules.models.Food
+import com.github.cawtoz.dineapi.modules.models.Menu
 import com.github.cawtoz.dineapi.modules.routes.crud.crudRoutes
 import io.ktor.server.application.Application
 
 fun Application.routesModule() {
     crudRoutes<Food>("/food", FoodDAO())
+    crudRoutes<Menu>("/menu", MenuDAO())
 }
